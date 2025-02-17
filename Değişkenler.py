59d80a1a9e4806cfee30088b70116ffaedbf1e47
KubraAr <151403216+KubraAr@users.noreply.github.com>
2025-02-18 00:01:00 +0300

    Add files via upload

---
+++ Değişkenler.py
@@ -0,0 +1,15 @@
+#Görev1
+isim = "Kübra" 
+yas = 19
+boy = 1.69
+ogrenci_mi = True
+
+print("İsim:", isim)
+print("Yaş:", yas)
+print("Boy:", boy)
+print("Öğrenci mi?:", ogrenci_mi)
+
+#Görev2
+dogum_yili = int(input("Doğum yilini gir: "))
+yas = 2025 - dogum_yili
+print("Yaşınız:", yas)
